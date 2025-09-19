be29bd11f1609ae092b84e0f27843c7e44ec7289
Jeffrey Bell <jeffbell924@gmail.com>
2025-09-19 11:40:41 -0700

    extract single game fn.  rename some vars.

--- lunar.c
+++ lunar.c
@@ -14,7 +14,7 @@
 // I - NextAlt - Intermediate altitude (miles)
 // J - NextV - Intermediate velocity (miles/sec)
 // K - Fuel rate (lbs/sec)
-// L - Elapsed time (sec)
+// L - Elapsed - Elapsed time (sec)
 // M - Mass -  Total weight (lbs)
 // N - Fuel - Empty weight (lbs, Note: M - N is remaining fuel weight)
 // S - Time elapsed in current 10-second turn (sec)
@@ -22,7 +22,7 @@
 // V - Downward speed (miles/sec)
 // Z - SpecificImpulse Thrust per pound of fuel burned
 
-static double Alt, NextAlt, NextV, K, L, Mass, Fuel, S, Timestep, V;
+static double Alt, NextAlt, NextV, K, Elapsed, Mass, Fuel, S, Timestep, V;
 
 // physical constants
 static const double G = .001;
@@ -33,6 +33,7 @@ static int echo_input = 0;
 static void update_lander_state();
 static void apply_thrust();
 
+static void play_a_game();
 static void report_landing();
 
 // Input routines (substitutes for FOCAL ACCEPT command)
@@ -51,114 +52,118 @@ int main(int argc, const char **argv)
             echo_input = 1;
     }
 
-    // Pint Preamble
+    // Print Preamble
     puts("CONTROL CALLING LUNAR MODULE. MANUAL CONTROL IS NECESSARY");
     puts("YOU MAY RESET FUEL RATE K EACH 10 SECS TO 0 OR ANY VALUE");
     puts("BETWEEN 8 & 200 LBS/SEC. YOU'VE 16000 LBS FUEL. ESTIMATED");
     puts("FREE FALL IMPACT TIME-120 SECS. CAPSULE WEIGHT-32500 LBS\n\n");
 
-    do // 01.20 in original FOCAL code
-    {
-        puts("FIRST RADAR CHECK COMING UP\n\n");
-        puts("COMMENCE LANDING PROCEDURE");
-        puts("TIME,SECS   ALTITUDE,MILES+FEET   VELOCITY,MPH   FUEL,LBS   FUEL RATE");
+    do {
+        play_a_game();
 
-        Alt = 120;
-        V = 1;
-        Mass = 32500;
-        Fuel = 16500;
-        L = 0;
-
-    start_turn: // 02.10 in original FOCAL code
-        printf("%7.0f%16.0f%7.0f%15.2f%12.1f      ",
-               L,
-               trunc(Alt),
-               5280 * (Alt - trunc(Alt)),
-               3600 * V,
-               Mass - Fuel);
-
-        prompt_for_k();
-
-        Timestep = 10;
-
-    turn_loop:
-        for (;;) // 03.10 in original FOCAL code
-        {
-            if (Mass - Fuel < .001)
-                goto fuel_out;
-
-            if (Timestep < .001)
-                goto start_turn;
-
-            S = Timestep;
-
-            if (Fuel + S * K - Mass > 0)
-                S = (Mass - Fuel) / K;
-
-            apply_thrust();
-
-            if (NextAlt <= 0)
-                goto loop_until_on_the_moon;
-
-            // Special case where were swoop.
-            // We reversed direction and started going back up.
-            if ((V > 0) && (NextV < 0))
-            {
-                for (;;) // 08.10 in original FOCAL code
-                {
-                    // FOCAL-to-C gotcha: In FOCAL, multiplication has a higher
-                    // precedence than division.  In C, they have the same
-                    // precedence and are evaluated left-to-right.  So the
-                    // original FOCAL subexpression `M * G / Z * K` can't be
-                    // copied as-is into C: `Z * K` has to be parenthesized to
-                    // get the same result.
-                    double W = (1 - Mass * G / (SpecificImpulse * K)) / 2;
-                    S = Mass * V / (SpecificImpulse * K * (W + sqrt(W * W + V / SpecificImpulse))) + 0.5;
-                    apply_thrust();
-                    if (NextAlt <= 0)
-                        goto loop_until_on_the_moon;
-                    update_lander_state();
-                    if (-NextV < 0)
-                        goto turn_loop;
-                    if (V <= 0)
-                        goto turn_loop;
-                }
-            }
-
-            update_lander_state();
-        }
-
-    loop_until_on_the_moon: // 07.10 in original FOCAL code
-        while (S >= .005)
-        {
-            S = 2 * Alt / (V + sqrt(V * V + 2 * Alt * (G - SpecificImpulse * K / Mass)));
-            apply_thrust();
-            update_lander_state();
-        }
-        goto on_the_moon;
-
-    fuel_out: // 04.10 in original FOCAL code
-        printf("FUEL OUT AT %8.2f SECS\n", L);
-        S = (sqrt(V * V + 2 * Alt * G) - V) / G;
-        V += G * S;
-        L += S;
-
-        // Report landing quality.
-    on_the_moon:
-        report_landing();
-
+        puts("CONTROL OUT\n\n");
         puts("\n\n\nTRY AGAIN?");
     } while (accept_yes_or_no());
-
-    puts("CONTROL OUT\n\n");
-
     return 0;
 } // main
+
+
+static void play_a_game(){
+    puts("FIRST RADAR CHECK COMING UP\n\n");
+    puts("COMMENCE LANDING PROCEDURE");
+    puts("TIME,SECS   ALTITUDE,MILES+FEET   VELOCITY,MPH   FUEL,LBS   FUEL RATE");
+
+    Alt = 120;
+    V = 1;
+    Mass = 32500;
+    Fuel = 16500;
+    Elapsed = 0;
+
+start_turn: // 02.10 in original FOCAL code
+    printf("%7.0f%16.0f%7.0f%15.2f%12.1f      ",
+            Elapsed,
+            trunc(Alt),
+            5280 * (Alt - trunc(Alt)),
+            3600 * V,
+            Mass - Fuel);
+
+    prompt_for_k();
+
+    Timestep = 10;
+
+turn_loop:
+    for (;;) // 03.10 in original FOCAL code
+    {
+        if (Mass - Fuel < .001)
+            goto fuel_out;
+
+        if (Timestep < .001)
+            goto start_turn;
+
+        S = Timestep;
+
+        if (Fuel + S * K - Mass > 0)
+            S = (Mass - Fuel) / K;
+
+        apply_thrust();
+
+        if (NextAlt <= 0)
+            goto loop_until_on_the_moon;
+
+        // Special case where were swoop.
+        // We reversed direction and started going back up.
+        if ((V > 0) && (NextV < 0))
+        {
+            for (;;) // 08.10 in original FOCAL code
+            {
+                // FOCAL-to-C gotcha: In FOCAL, multiplication has a higher
+                // precedence than division.  In C, they have the same
+                // precedence and are evaluated left-to-right.  So the
+                // original FOCAL subexpression `M * G / Z * K` can't be
+                // copied as-is into C: `Z * K` has to be parenthesized to
+                // get the same result.
+                double W = (1 - Mass * G / (SpecificImpulse * K)) / 2;
+                S = Mass * V / (SpecificImpulse * K * (W + sqrt(W * W + V / SpecificImpulse))) + 0.5;
+                apply_thrust();
+                if (NextAlt <= 0)
+                    goto loop_until_on_the_moon;
+                update_lander_state();
+                if (-NextV < 0)
+                    goto turn_loop;
+                if (V <= 0)
+                    goto turn_loop;
+            }
+        }
+
+        update_lander_state();
+    }
+
+loop_until_on_the_moon: // 07.10 in original FOCAL code
+    while (S >= .005)
+    {
+        S = 2 * Alt / (V + sqrt(V * V + 2 * Alt * (G - SpecificImpulse * K / Mass)));
+        apply_thrust();
+        update_lander_state();
+    }
+    goto on_the_moon;
+
+fuel_out: // 04.10 in original FOCAL code
+    printf("FUEL OUT AT %8.2f SECS\n", Elapsed);
+    S = (sqrt(V * V + 2 * Alt * G) - V) / G;
+    V += G * S;
+    Elapsed += S;
+
+    // Report landing quality.
+on_the_moon:
+    report_landing();
+    }
+
+
 
 // Subroutine at line 06.10 in original FOCAL code
 void update_lander_state()
 {
-    L += S;
+    Elapsed += S;
     Timestep -= S;
     Mass -= S * K;
     Alt = NextAlt;
@@ -168,6 +173,7 @@ void update_lander_state()
 // Subroutine at line 09.10 in original FOCAL code
 void apply_thrust()
 {
+    // Taylor series?
     double Q = S * K / Mass;
     double Q_2 = pow(Q, 2);
     double Q_3 = pow(Q, 3);
@@ -180,29 +186,26 @@ void apply_thrust()
 
 void report_landing()
 {
-    printf("ON THE MOON AT %8.2f SECS\n", L);
-    double W = 3600 * V;
-    printf("IMPACT VELOCITY OF %8.2f M.P.H.\n", W);
+    printf("ON THE MOON AT %8.2f SECS\n", Elapsed);
+    double Mph = 3600 * V;
+    printf("IMPACT VELOCITY OF %8.2f M.P.H.\n", Mph);
     printf("FUEL LEFT: %8.2f LBS\n", Mass - Fuel);
-    if (W <= 1)
+    if (Mph <= 1)
         puts("PERFECT LANDING !-(LUCKY)");
-    else if (W <= 10)
+    else if (Mph <= 10)
         puts("GOOD LANDING-(COULD BE BETTER)");
-    else if (W <= 22)
+    else if (Mph <= 22)
         puts("CONGRATULATIONS ON A POOR LANDING");
-    else if (W <= 40)
+    else if (Mph <= 40)
         puts("CRAFT DAMAGE. GOOD LUCK");
-    else if (W <= 60)
+    else if (Mph <= 60)
         puts("CRASH LANDING-YOU'VE 5 HRS OXYGEN");
     else
     {
         puts("SORRY,BUT THERE WERE NO SURVIVORS-YOU BLEW IT!");
-        printf("IN FACT YOU BLASTED A NEW LUNAR CRATER %8.2f FT. DEEP\n", W * .277777);
+        printf("IN FACT YOU BLASTED A NEW LUNAR CRATER %8.2f FT. DEEP\n", Mph * .277777);
     }
 }
-
-
-
 
 // Give some hints.
 void prompt_for_k(){
